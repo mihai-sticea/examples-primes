549ac5bacc8e08be3cc94efa88debf69f166e127
Mihai Sticea <mihai.sticea@mail.mcgill.ca>
2013-10-02 11:36:40 -0400

    Changed variables name to be more meaningful
    The following changes have been done in the code:
    RR -             numberOfRows
    CC -            numberOfColumns
    ORDMAX - sizeOfHelperArray
    JPRIME -    numberIsPrime
    N -              positionInHelperArray
    MULT -       helperArray
    J -                oddNumber
    ORD -         primeNumberToBeSquared
    SQUARE -  nextSquaredPrime
    C -              i

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -1,16 +1,16 @@
 public class PrintPrimes {
   int numberOfPrimes;
-  int RR;
-  int CC;
-  int ORDMAX;
+  int numberOfRows;
+  int numberOfColumns;
+  int sizeOfHelperArray;
   int listOfPrimes[];
 
-  public PrintPrimes(int numberOfPrimes, int RR, int CC, int ORDMAX) {
-    this.numberOfPrimes   = numberOfPrimes;
-    this.RR  = RR;
-    this.CC  = CC;
+  public PrintPrimes(int numberOfPrimes, int numberOfRows, int numberOfColumns, int sizeOfHelperArray) {
+    this.numberOfPrimes = numberOfPrimes;
+    this.numberOfRows = numberOfRows;
+    this.numberOfColumns = numberOfColumns;
 
-    this.ORDMAX = ORDMAX;
+    this.sizeOfHelperArray = sizeOfHelperArray;
     this.listOfPrimes = new int[numberOfPrimes + 1];
   }
 
@@ -23,42 +23,42 @@ public class PrintPrimes {
 
   public void calculatePrimes() {
       /* Two is the only even prime. All other prime numbers are odd.
-       * To simplify the code, we simply add 2 as a prime number, and
-       * delegate the task of finding all odd prime numbers to a helper
-       * function.
-       */
+* To simplify the code, we simply add 2 as a prime number, and
+* delegate the task of finding all odd prime numbers to a helper
+* function.
+*/
       listOfPrimes[1] = 2;
       calculateOddPrimes();
   }
 
   private void calculateOddPrimes() {
-      boolean JPRIME;
-      int N;
-      int MULT[] = new int[ORDMAX + 1];
+      boolean numberIsPrime;
+      int positionInHelperArray;
+      int helperArray[] = new int[sizeOfHelperArray + 1];
 
-      int J = 1;
-      int ORD = 2;
-      int SQUARE = 9;
+      int oddNumber = 1;
+      int primeNumberToBeSquared = 2;
+      int nextSquaredPrime = 9;
 
       for(int primesFoundSoFar = 2; primesFoundSoFar <= numberOfPrimes; primesFoundSoFar++) {
         do {
-          J = J + 2;
-          if (J == SQUARE) {
-            ORD = ORD + 1;
-            SQUARE = listOfPrimes[ORD] * listOfPrimes[ORD];
-            MULT[ORD - 1] = J;
+          oddNumber = oddNumber + 2;
+          if (oddNumber == nextSquaredPrime) {
+            primeNumberToBeSquared = primeNumberToBeSquared + 1;
+            nextSquaredPrime = listOfPrimes[primeNumberToBeSquared] * listOfPrimes[primeNumberToBeSquared];
+            helperArray[primeNumberToBeSquared - 1] = oddNumber;
           }
-          N = 2;
-          JPRIME = true;
-          while (N < ORD && JPRIME) {
-            while (MULT[N] < J)
-              MULT[N] = MULT[N] + listOfPrimes[N] + listOfPrimes[N];
-            if (MULT[N] == J)
-              JPRIME = false;
-            N = N + 1;
+          positionInHelperArray = 2;
+          numberIsPrime = true;
+          while (positionInHelperArray < primeNumberToBeSquared && numberIsPrime) {
+            while (helperArray[positionInHelperArray] < oddNumber)
+              helperArray[positionInHelperArray] = helperArray[positionInHelperArray] + listOfPrimes[positionInHelperArray] + listOfPrimes[positionInHelperArray];
+            if (helperArray[positionInHelperArray] == oddNumber)
+              numberIsPrime = false;
+            positionInHelperArray = positionInHelperArray + 1;
           }
-        } while (!JPRIME);
-        listOfPrimes[primesFoundSoFar] = J;
+        } while (!numberIsPrime);
+        listOfPrimes[primesFoundSoFar] = oddNumber;
       }
     }
 
@@ -69,17 +69,15 @@ public class PrintPrimes {
           System.out.println("The First " + numberOfPrimes +
                                " Prime Numbers --- Page " + pageNumber);
           System.out.println("");
-          for (int rowOffset = pageOffset; rowOffset < pageOffset + RR; rowOffset++){
-            for (int C = 0; C < CC;C++)
-              if (rowOffset + C * RR <= numberOfPrimes)
-                System.out.format("%10d", listOfPrimes[rowOffset + C * RR]);
+          for (int rowOffset = pageOffset; rowOffset < pageOffset + numberOfRows; rowOffset++){
+            for (int i = 0; i < numberOfColumns;i++)
+              if (rowOffset + i * numberOfRows <= numberOfPrimes)
+                System.out.format("%10d", listOfPrimes[rowOffset + i * numberOfRows]);
             System.out.println("");
           }
           System.out.println("\f");
           pageNumber = pageNumber + 1;
-          pageOffset = pageOffset + RR * CC;
+          pageOffset = pageOffset + numberOfRows * numberOfColumns;
         }
     }
-}
-
-					 
+}	 
